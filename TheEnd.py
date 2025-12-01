65110891f709905ccc52cfdd6d80584bca593a47
mac <mac@mac.local>
2025-12-02 04:24:26 +0800

    Add TheEnd.py plot script

---
+++ TheEnd.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+plt.figure(figsize=(6, 3))
+
+
+plt.text(
+    0.5, 0.5,
+    "The END",
+    fontsize=60,
+    ha='center',
+    va='center',
+    fontweight='bold'
+)
+
+
+plt.axis('off')
+
+
+plt.tight_layout()
+plt.show()
